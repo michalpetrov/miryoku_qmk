0a79b1e0c4ad2eb88ba47cedc88b88d5daed4858
Michal Petrov <michal.petrov@mtrust.eu>
2024-03-17 23:57:27 +0100

    aa

--- keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
@@ -31,7 +31,6 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 void oled_render_layer_state(void) {
     oled_write_P(PSTR("Layer: "), false);
-    oled_write_ln_P(PSTR(layer_state), false);
     switch (layer_state) {
         case 0:
             oled_write_ln_P(PSTR("Base"), false);
